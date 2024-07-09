c48dd3478ab171e1d4fb67e807236c47acd46d8d
ANANYA_SADHIYE <155825109+ananyasadhiye@users.noreply.github.com>
2024-07-10 01:20:22 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,21 @@
+// Function to open modal
+function openModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "block";
+}
+
+// Function to close modal
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "none";
+}
+
+// Close modal when clicking outside of it
+window.onclick = function(event) {
+    var modals = document.getElementsByClassName('modal');
+    for (var i = 0; i < modals.length; i++) {
+        if (event.target == modals[i]) {
+            modals[i].style.display = "none";
+        }
+    }
+}
